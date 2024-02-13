eeadb8b8cfcf8361f0f32fcaa29ae57b136f1c25
Wael Karman <wael.karman@gmail.com>
2024-02-13 16:06:06 +0100

    init

--- main_exceptions.cpp
+++ main_exceptions.cpp
@@ -1,7 +1,104 @@
 #include <iostream>
 #include <exception>
 
+/*
+delle volte è possibile aggiungere un if per controllare un comportamento atteso altre voltre come nel caso di scanf che ritorna un il numero di caratteri letti oppure EOF
+basta controllare il valore di ritorno che spesso viene omesso. Delle volte la gestione o non si può fare in loco perche magari esiste una dipendenza esterna
+di solito si usa il valore di ritorno delle funzioni come feedback e si passa alla funzione un reference per portare fuori il risutlato. Questo però non vale sempre
+perche per esempio i costruttori non possono tornare nulla al di fuori del puntatore all oggetto. Ma comuqnue tutto questo discorso vale per gli errori attesi e controllati con
+un if di cui si ritorna codice di errore.
+GLi errori inattesi sono quelli che si sa che possono accadere ma non ci si aspetta che avvengano. Invece di aggiungere mille controlli perche mille sono i possibilimotivi di fallimento
+per ogni volta che quel pezzo di codice viene eseguito cosa che rallenta il programma anche se funziona bene. Poi c'è il problema di cosa fare in caso in caso di errore.se gestito
+in un if else nell else ci sarà un casino di roba probabilmente.
+GLi errori attesi sono quasi sempre facilmente gestibili.
+
+un eccezione si lancia con "throw" e si può lanciare qualsiasi oggetto se si estende exception si accoda alla standard libary ma non è dettoche bisogna farlo. cio provoca l interruzione del flusso di esecuzione e si passa a gestire l exception
+Il tipo (nel catch) di dato viene usato per capire come gestire, il valore descrive meglio. quindi se fai throw usi un oggetto e quel tipo di oggetto viene usato nel
+"catch" per capire che è sudccesso
+esiste una classe std::exception con le sue sottoclassi fatta apposta per queste cose
+Le ecezioni si gesticono con blocchi try{}catch(){}catch(exception& e){} supponiamo questo accada dentro una fuznione e che i 2 catch non siano in grado di
+gestire l' eccezione, quello che accade è l'uscita dalla funzione con la distruzione del contesto uscendo di scope. Si torna dal chiamante e si vede se lui
+sa gestire l'eccezione o no risalendo nella catena di chiamate fino a cancellare l esecuzione corrente. L'unico oggetto che resta vivo è quello creato nel catch
+di cui si fa copia (e quindi deve supportare la copia in caso di eccezione custom)
+il codice riprende dopo il blocco try-catch una volta eseguito il codice in catch
+
+catch(...){} significa qualunque eccezione sia fa questo
+in un blocco catch per rilanciare l eccezione stessa si usa solo "throw;"
+
+Nel catch fra tonde si mette l' oggetto di cui si è fatto throw tale. E' bene mettere nel catch un reference all oggetto (che viene passato al catch un po come il passaggio a una funzione)
+in modo che se l oggetto non è proprio quello specificato avendo indietro un puntatore non si perde l' informazione sul tipo originale perche metto un oggetto derivato in un eccezione base
+e avviene lo slicing ovvero una parte dell oggetto viene copiata e un parte viene persa essendo tale classe appunto una classe base.
+
+si puo creare una classe che importa exception ed eredita da exceprion che ridefinisce what() utile per creare eccezioni oppure si usano quelle predefinite
+
+c++ non supporta finally perche lo stack si contrae e vengono chiamati i distruttori degli oggetti.
+*/
+
+
 using namespace std;
+
+
+class exc_custom_pure0:public exception{
+    string a;
+public:
+    exc_custom_pure0(){}
+
+    exc_custom_pure0(string n):a(n){
+        cout<<"exc_custom_pure0 LAUNCHED"<< endl;
+    }
+    exc_custom_pure0(const exc_custom_pure0& ecp){
+        a=ecp.get_a();
+    }
+    string get_a() const {
+        return a;
+    }
+    const char * what(){
+        return "exception catched guess type and message!";
+    }
+
+};
+
+class exc_custom_pure{
+    string a;
+public:
+    exc_custom_pure(){}
+
+    exc_custom_pure(string n):a(n){
+        cout<<"exc_custom_pure LAUNCHED"<< endl;
+    }
+    exc_custom_pure(const exc_custom_pure& ecp){
+        a=ecp.get_a();
+    }
+    string get_a() const {
+        return a;
+    }
+
+};
+
+class exc_custom:public exception{
+public:
+    exc_custom(){
+        cout<<"exc_custom build"<< endl;
+    }
+
+    const char* what(){
+        return "exc_custom LAUNCHED";
+    }
+};
+
+class exc_custom_test:public exception{
+public:
+    exc_custom_test(){
+        cout<<"unknown build"<< endl;
+    }
+
+    const char* what(){
+        return "unknown LAUNCHED";
+    }
+};
+
+
+
 
 class Base0{
 public:
@@ -29,6 +126,12 @@ public:
         cout<< a << endl;
     }
     void stampa1(){
+        try{
+            throw exc_custom_pure0(a);
+        }
+        catch(out_of_range& e){
+            cout << e.what() << " -> out_of_range" << endl;
+        }
         cout<< a << endl;
     }
     virtual void stampa2(){
@@ -46,6 +149,20 @@ public:
     }
 
     void stampa0() override {
+        try{
+            throw exc_custom_test();
+        }
+        catch(...){
+            cout << "Unknown inner exception." << std::endl;
+        }
+
+        try{
+            throw exc_custom_test();
+        }
+        catch(out_of_range e){
+            cout << e.what() << " -> exc_custom_test" << endl;
+        }
+
         cout<< a << endl;
     }
     void stampa1(){
@@ -58,38 +175,135 @@ public:
     }
 };
 
-class Derived2: public Base{
-public:
-    string a="456";
-
-    void stampa0() {
-        cout<< a << endl;
-    }
-    void stampa1(){
-        cout<< a << endl;
-    }
-    void stampa2() {
-        cout<< a <<endl;
-    }
-};
 
 int main()
 {
+    int test=0;
     Base *b1 = new Derived1("777","888");
-    Base *b2 = new Derived2();
 
-    b1->stampa0();
-    b2->stampa0();
+    cout << "<- "<< ++test << endl << endl;
 
-    b1->stampa1();
-    b2->stampa1();
+    try{
+        b1->stampa0();
+    }
+    catch(...){
+        cout << "Unknown outer exception." << std::endl;
+    }
 
-    b1->stampa2();
-    b2->stampa2();
+    cout << "<- "<< ++test << endl << endl;
+
+    try{
+        b1->stampa1();
+    }
+    catch(exc_custom_pure0& e){
+        cout << typeid(e).name() << std::endl;
+        cout << e.what() << std::endl;
+        cout << "return copied value  - " << e.get_a() << endl << endl ;
+    }
+    //catch(exception e){
+    //    cout << typeid(e).name() << std::endl;
+    //    cout << e.what() << std::endl;
+    //    auto ee = static_cast<exc_custom_pure0>(e);
+    //    cout << " - " << ee.get_a() << endl << endl ;
+    //}
 
 
-    cout << "ECCEZIONI:" << endl;
-    throw std::out_of_range("Indice fuori dal range valido");
+
+    cout << "---Exceptions---" << endl;
+
+    cout << "<- "<< ++test << endl << endl;
+
+    try{
+        throw exc_custom();
+    }
+    catch(out_of_range e){
+        cout << e.what() << " -> out_of_range" << endl;
+    }
+    catch(exc_custom e){
+        cout << e.what() << " -> exc_custom" << endl;
+    }
+    catch(...){
+        cout << "Unknown exception ." << std::endl;
+    }
+
+    cout << "<- "<< ++test << endl << endl;
+
+    try{
+        throw exc_custom_pure();
+    }
+    catch(out_of_range e){
+        cout << e.what() << " -> out_of_range" << endl;
+    }
+    catch(exc_custom_pure e){
+        cout << " -> exc_custom_pure" << endl;
+    }
+    catch(...){
+        cout << "Unknown exception ." << std::endl;
+    }
+
+    cout << "<- "<< ++test << endl << endl;
+
+    try{
+        throw std::out_of_range("Index out of range");
+    }
+    catch(out_of_range e){
+        cout << e.what() << " -> out_of_range" << endl;
+    }
+    catch(exception e){
+        cout << e.what() << " -> exception" << endl;
+    }
+    catch(...){
+        cout << "Unknown exception ." << std::endl;
+    }
+
+    cout << "<- "<< ++test << endl << endl;
+
+    try{
+        throw std::out_of_range("Index out of range");
+    }
+    catch(exception e){
+        cout << e.what() << " -> exception" << endl;
+    }
+    catch(out_of_range e){
+        cout << e.what() << " -> out_of_range" << endl;
+    }
+    catch(...){
+        cout << "Unknown exception ." << std::endl;
+    }
+
+    cout << "<- "<< ++test << endl << endl;
+
+    try{
+        throw exc_custom();
+    }
+    catch(out_of_range e){
+        cout << e.what() << " -> out_of_range" << endl;
+    }
+    catch(exc_custom_pure e){
+        cout << " -> exc_custom_pure" << endl;
+    }
+    catch(...){
+        cout << "Unknown exception." << std::endl;
+    }
+
+    cout << "<- "<< ++test << endl << endl;
+
+    try{
+        throw exc_custom_test();
+    }
+    catch(exception& e){
+        cout << e.what() << " ->GENERAL EXCEPTION" << endl;
+        cout << typeid(e).name() << std::endl;
+    }
+    catch(out_of_range& e){
+        cout << " -> out_of_range" << endl;
+    }
+    catch(...){
+        cout << "Unknown exception exit execution." << std::endl;
+    }
+
+    cout << "<- "<< ++test << endl << endl;
+
 
     return 0;
 }
--- main_theory.cpp
+++ main_theory.cpp
@@ -183,38 +183,6 @@ int main()
     //gli elementi dell oggetto. Quando si fa un cast si ridefinisce il puntatore con le aree di memoria in cui confinato senza modificare la memoria iniziale
     //quindi un cast nel caso del C che lascia immutato il puntatore reinterpretando la memoria è diverso da un cast corretto eseguito in c++
 
-    cout << "ECCEZIONI:" << endl;
-    //delle volte è possibile aggiungere un if per controllare un comportamento atteso altre voltre come nel caso di scanf che ritorna un il numero di caratteri letti oppure EOF
-    //basta controllare il valore di ritorno che spesso viene omesso. Delle volte la gestione o non si può fare in loco perche magari esiste una dipendenza esterna
-    //di solito si usa il valore di ritorno delle funzioni come feedback e si passa alla funzione un reference per portare fuori il risutlato. Questo però non vale sempre
-    //perche per esempio i costruttori non possono tornare nulla al di fuori del puntatore all oggetto. Ma comuqnue tutto questo discorso vale per gli errori attesi e controllati con
-    //un if di cui si ritorna codice di errore.
-    //GLi errori inattesi sono quelli che si sa che possono accadere ma non ci si aspetta che avvengano. Invece di aggiungere mille controlli perche mille sono i possibilimotivi di fallimento
-    //per ogni volta che quel pezzo di codice viene eseguito cosa che rallenta il programma anche se funziona bene. Poi c'è il problema di cosa fare in caso in caso di errore.se gestito
-    //in un if else nell else ci sarà un casino di roba probabilmente.
-    //GLi errori attesi sono quasi sempre facilmente gestibili.
-
-    //un eccezione si lanci acon "throw" e si può lanciare su qualsiasi tipo. cio provoca l interruzione del flusso di esecuzione e si passa a gestire l exception
-    //Il tipo di dato viene usato per capire come gestire, il valore descrive meglio. quindi se fai throw usi un oggetto e quel tipo di oggetto viene usato nel
-    //"catch" per capire che è sudccesso
-    //esiste una classe std::exception con le sue sottoclassi fatta apposta per queste cose
-    //Le ecezioni si gesticono con blocchi try{}catch(){}catch(exception& e){} supponiamo questo accada dentro una fuznione e che i 2 catch non siano in grado di
-    //gestire l' eccezione, quello che accade è l'uscita dalla funzione con la distruzione del contesto uscendo di scope. Si torna dal chiamante e si vede se lui
-    //sa gestire l'eccezione o no risalendo nella catena di chiamate fino a cancellare l esecuzione corrente. L'unico oggetto che resta vivo è quello creato nel catch
-    //di cui si fa copia (e quindi deve supportare la copia in caso di eccezione custom)
-    //il codice riprende dopo il blocco try-catch una volta eseguito il codice in catch
-
-    //catch(...){} significa qualunque eccezione sia fa questo
-    //in un blocco catch per rilanciare l eccezione stessa si usa solo "throw;"
-
-    //Nel catch fra tonde si mette l' oggetto di cui si è fatto throw tale. E' bene mettere nel catch un reference all oggetto (che viene passato al catch un po come il passaggio a una funzione)
-    //in modo che se l oggetto non è proprio quello specificato avendo indietro un puntatore non si perde l' informazione sul tipo originale perche metto un oggetto derivato in un eccezione base
-    //e avviene lo slicing ovvero una parte dell oggetto viene copiata e un parte viene persa essendo tale classe appunto una classe base.
-
-    //si puo creare una classe che importa exception ed eredita da exceprion che ridefinisce what() utile per creare eccezioni oppure si usano quelle predefinite
-
-    //c++ non supporta finally perche lo stack si contrae e vengono chiamati i distruttori degli oggetti.
-
     throw std::out_of_range("Indice fuori dal range valido");
 
     cout << "SMART POINTER:" << endl;
