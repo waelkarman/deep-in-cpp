fbeeba67ebc4f99f9eed7e27457a509f215727d4
Wael Karman <wael.karman@gmail.com>
2024-02-14 15:06:48 +0100

    init

--- main_tricks.cpp
+++ main_tricks.cpp
@@ -8,6 +8,10 @@ int main(){
     //variant
     //forwarding
     // explicit, inline, volatile,
+    // un metodo definito const non modifica lo stato dell oggetto se const è abbinato ad un reference permette di evitare la copia tenendo immutato l oggetto passato
+    // override serve a specificare che si fa un override e quindi il compilatore eseguirà un marching dei tipi
+    // noexcept dice al compilatore che una funzione non lancerà eccezioni questo permette al compilatore di ottimizzare meglio il codice perche sa di non dover gestire eccezioni
+    //slicing per slicing si intende la copia di qualcosa in uno spazio piu piccolo che è consentita senza problemi ma non viene tutto effettivamente copiato ma solo la parte che ci entra con la conseguente perdita di informazione
 
 };
 
