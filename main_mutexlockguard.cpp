16b7e56451943b256a621d5ce55ad1d967ded24a
Wael Karman <wael.karman@gmail.com>
2024-02-18 00:42:32 +0100

    init

--- main_mutexlockguard.cpp
+++ main_mutexlockguard.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 /*
-in alcuni casi la necessità non è aspettare la disponibilità di un dato piuttosto quella di essere sicuro che nessun altro stia facendo una certa cosa
-quindi non la presenza di qualcosa ma l' assenza !!!
+in alcuni casi la necessità non è aspettare la disponibilità di un dato piuttosto quella di essere sicuro che nessun altro stia facendo
+una certa cosa quindi non la presenza di qualcosa ma l' assenza !!!
 
 in questo caso di usano i mutex che permettono di avere accesso controllato ad una frazione di codice mutual exclusion
 esempio si sta usando una linked list bisogna rompere i puntatori aggiungere qualcosa e poi ricollegarli se qualcun altro
@@ -18,16 +18,17 @@ la chiamata di unlock è sempre a carico del programamtore anche se il codice si
 il mutex non ha ancuna corrispondenza con quanto protegge è tutto a carico del programmatore e quindi va documentata bene
 il mutex serve in lettura e scrittura perche se no potrei leggere scritture parziali che sono fatte al contempo
 
-se chiamo due volte lock() attendo per sempre perche davanti ad un lock non rilasciato si attende che qualcuno lo rilasci ma se a doverlo rilasciare
+se chiamo due volte lock() attendo per sempre perche davanti ad un lock non rilasciato si attende che qualcuno lo rilasci ma se a doverlo
+rilasciare
 sono io che sono bloccato nell attesa questo non avverrà mai -> deadlock
 
 esiste anche std::recursive_mutex per gestire questa cosa ma alla chiamata di due lock servono 2 unlock
 esiste anche un metodo try_lock che ritorna un booleano evitando di rimanere bloccati nell attesa di un mutex
 
 è molto raro usare lock su un mutex direttamnete, in genere dal mutex si crea un oggetto std::lock_guard<lockable> prende un mutex in ingresso
-il lock_guard è un oggetto che ha un lock nel costruttore e un unlock nel distruttore nel caso un eccezione avvenga nel blocco loccato la contrazione dello stack
-fa sbloccare il mutex. se si costruisce questo oggetto lock_guard in un metodo si assicura che quando la graffa del metodo sarà chiusa il lock sarà rilasciato
-quindi paradigma RAII reg12 1:23:16
+il lock_guard è un oggetto che ha un lock nel costruttore e un unlock nel distruttore nel caso un eccezione avvenga nel blocco loccato la
+contrazione dello stack fa sbloccare il mutex. se si costruisce questo oggetto lock_guard in un metodo si assicura che quando la graffa del
+metodo sarà chiusa il lock sarà rilasciato quindi paradigma RAII reg12 1:23:16
 se definisco un mutex dentro una classe per quell oggetto userò sempre lo stesso mutex anche se passo l oggetto a piu thread
 */
 
