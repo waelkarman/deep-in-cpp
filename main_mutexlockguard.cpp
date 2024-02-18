dd37de4d8a611885220b1ddba991018cd0a89866
Wael Karman <wael.karman@gmail.com>
2024-02-18 13:15:36 +0100

    init

--- main_mutexlockguard.cpp
+++ main_mutexlockguard.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <future>
+#include <vector>
 
 using namespace std;
 /*
 in alcuni casi la necessità non è aspettare la disponibilità di un dato piuttosto quella di essere sicuro che nessun altro stia facendo
-una certa cosa quindi non la presenza di qualcosa ma l' assenza !!!
+una certa cosa quindi non la presenza di qualcosa ma l' assenza !!
 
 in questo caso di usano i mutex che permettono di avere accesso controllato ad una frazione di codice mutual exclusion
 esempio si sta usando una linked list bisogna rompere i puntatori aggiungere qualcosa e poi ricollegarli se qualcun altro
@@ -13,7 +17,7 @@ allora chi deve fare operazioni di questo tipo su linked list assume il lock e p
 tutti devono usare lo stesso mutex creato per lo scopo dal programmatore
 le operazioni di lock e unlock includono dei full memory fence che gestiscono i problemi di sincronizzazione fra thread.
 
-la chiamata di unlock è sempre a carico del programamtore anche se il codice si ferma per un eccezione bisogna prevedere tutto
+la chiamata di unlock è sempre a carico del programmatore anche se il codice si ferma per un eccezione bisogna prevedere tutto
 
 il mutex non ha ancuna corrispondenza con quanto protegge è tutto a carico del programmatore e quindi va documentata bene
 il mutex serve in lettura e scrittura perche se no potrei leggere scritture parziali che sono fatte al contempo
@@ -32,7 +36,52 @@ metodo sarà chiusa il lock sarà rilasciato quindi paradigma RAII reg12 1:23:16
 se definisco un mutex dentro una classe per quell oggetto userò sempre lo stesso mutex anche se passo l oggetto a piu thread
 */
 
-int main(){
-    cout<<"HELLO";
+
+// Thread Safe class
+class prod_cons{
+    std::mutex mtx;
+    int valore = 0;
+public:
+
+
+    void increase(int n){
+        std::lock_guard<std::mutex> guard(mtx);
+        valore+=n;
+        cout << "Valore aumentato: " << valore << endl;
+    }
+
+    void decrease(int n){
+        std::lock_guard<std::mutex> guard(mtx);
+        valore-=n;
+        cout << "Valore diminuito: " << valore << endl;
+    }
 };
+
+
+void incr(prod_cons& p){
+    for(int i = 0 ; i < 100 ; i++){
+        p.increase(2);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        //std::this_thread::yield();
+    }
+}
+
+void decr(prod_cons& p){
+    for(int i = 0 ; i < 100 ; i++){
+        p.decrease(2);
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        //std::this_thread::yield();
+    }
+}
+
+int main() {
+    prod_cons p;
+    std::thread t0(incr,std::ref(p));
+    std::thread t1(decr,std::ref(p));
+
+
+    t0.join();
+    t1.join();
+    return 0;
+}
 
