076d0dabf8a4168f2c31f1f118581962081b48de
Wael Karman <wael.karman@gmail.com>
2024-04-06 23:39:39 +0200

    clean

--- main_threadpooling.cpp
+++ main_threadpooling.cpp
@@ -1,4 +1,11 @@
+#include <functional>
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <condition_variable>
+#include <random>
+#include <atomic>
 
 using namespace std;
 
@@ -39,7 +46,74 @@ pool utilizzando le primitive di concorrenza fornite da C++11 (come std::thread,
  librerie di terze parti che offrono implementazioni di thread pool.
 */
 
-int main(){
-    cout<<"HELLO";
-};
+/**
+ * Thread pool draft
+ * threads creation overhead still not managed
+ *
+*/
 
+std::mutex mtx;
+std::condition_variable cv;
+bool ready = false;
+std::mutex queue_mutex;
+std::atomic<int> i(0);
+
+
+unsigned long long fibonacci_recursive(int n) {
+    if (n <= 1) return n;
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
+}
+
+void th_lodable(int k, vector<std::function<void(int)>> & tasks){
+    std::function<void(int)> f;
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        if (tasks.empty()){
+            return;
+        }
+        f = tasks.back();
+        tasks.pop_back();
+    }
+    f(k);
+    i--;
+}
+
+int main() {
+    unsigned int n = std::thread::hardware_concurrency();
+    std::cout << "Total available Cores: " << n << endl;
+
+    vector<thread> workers;
+    vector<std::function<void(int)>> tasks;
+
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(40, 46);
+
+    for(int q=0; q<100; q++){
+        int fib = distrib(gen);
+        std::function<void(int)> task = [=](int k){
+            cout << "execute task " << q << "on thread num: "<< k << endl;
+            fibonacci_recursive(fib);
+        };
+
+        tasks.push_back(std::move(task));
+    }
+
+    int k(0);
+    while(tasks.size()>0){
+        while(tasks.size()>0 && i<n){
+            k++;
+            workers.push_back(thread(th_lodable,k,std::ref(tasks)));
+            i++;
+        }
+    }
+
+    for(thread& w : workers){
+        if(w.joinable()){
+            w.join();
+        }
+    }
+
+    return 0;
+}
